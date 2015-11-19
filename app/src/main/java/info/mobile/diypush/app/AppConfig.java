f8e50e6fcfe2486c2dba676061ffd63d672a98ee
Vydeshramkumar <vydeshramkumar@live.com>
2015-11-19 17:06:37 +0530

    Parse push implemented using cloud code

---
+++ app/src/main/java/info/mobile/diypush/app/AppConfig.java
@@ -0,0 +1,11 @@
+package info.mobile.diypush.app;
+
+/**
+ * Created by Ravi on 15/05/15.
+ */
+public class AppConfig {
+    public static final String PARSE_CHANNEL = "DIYCHANNEL";
+    public static final String PARSE_APPLICATION_ID = "kYEpZrKeO9v7bFYJEddEgwNT3BuXiFK4C9Xnjt5a";
+    public static final String PARSE_CLIENT_KEY = "dPowKFiVM5YCJbk88xNOsjxFH2b5p1lk3eZDTlXn";
+    public static final int NOTIFICATION_ID = 100;
+}
---
+++ app/src/main/java/info/mobile/diypush/model/Message.java
@@ -0,0 +1,31 @@
+package info.mobile.diypush.model;
+
+
+public class Message {
+    private String message;
+    private long timestamp;
+
+    public Message() {
+    }
+
+    public Message(String message, long timestamp) {
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
